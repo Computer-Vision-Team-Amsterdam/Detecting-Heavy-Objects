a2089e65fafa01a609b228d5c9644d90a5850ddf
Diana Epureanu <dianaepureanu@Dianas-MacBook-Pro.local>
2021-11-15 17:45:14 +0100

    dummy model works on gpu

--- run_on_azure.py
+++ run_on_azure.py
@@ -1,7 +1,3 @@
-# The download location can be retrieved from argument values
-import sys
-
-# The download location can also be retrieved from input_datasets of the run context.
 from azureml.core import (
     ComputeTarget,
     Dataset,
@@ -12,28 +8,12 @@ from azureml.core import (
     ScriptRunConfig,
     Workspace,
 )
-from azureml.core.runconfig import DockerConfiguration
 from azureml.widgets import RunDetails
 
 if __name__ == "__main__":
     ws = Workspace.from_config()
 
-    """
-    # Create a Python environment for the experiment
-    env = Environment.from_conda_specification(name="detectron2",
-                                                       file_path="detectron2.yml")
-
-    # for some reason the python version is not automatically retrieved from yml file
-    env.python.conda_dependencies.set_python_version("3.8.5")
-    """
-
-    env = Environment.from_docker_image(
-        name="test-heavy",
-        image="bc4a4503f5dc4f37b696e129c206fbc8.azurecr.io/test-heavy",
-    )
-
-    env.inferencing_stack_version = "latest"
-    env.python.user_managed_dependencies = False
+    env = Environment.from_dockerfile("cuda_env_container", "Dockerfile")
 
     # Get datastore path
     default_ds: Datastore = ws.get_default_datastore()
@@ -43,12 +23,6 @@ if __name__ == "__main__":
         path=(default_ds, "UI/11-03-2021_070053_UTC/data/train/"), validate=False
     )
 
-    # TODO: check if create_new_version should stay True since it's the same data everytime for now
-    """
-    container_data = data_set.register(workspace=ws,
-                                       name='container-data',
-                                       description='container-data')
-    """
     dataset = Dataset.get_by_name(ws, "container-data")
 
     downloaded = dataset.as_download(path_on_compute="data/")
@@ -72,4 +46,4 @@ if __name__ == "__main__":
 
     # add if statement for registering
     run.wait_for_completion(show_output=True)
-    # run.register_model("test_model", f"outputs/exp0_{experiment_name}/weights/best.pt")
+    # run.register_model("test_model", f"outputs/model_final.pth")
