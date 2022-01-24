5b80fa76e52b58065675daaedfeb65f39a3e9da2
Diana Epureanu <dianaepureanu@Dianas-MacBook-Pro.local>
2022-01-24 10:59:07 +0200

    disable import

--- visualizations/daily_trajectory.py
+++ visualizations/daily_trajectory.py
@@ -12,8 +12,8 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import folium
 from folium.plugins import MarkerCluster
-from panorama import models # type: ignore
-from panorama.client import PanoramaClient # type: ignore
+from panorama import models  # pylint: disable=import-error
+from panorama.client import PanoramaClient  # pylint: disable=import-error
 
 
 @dataclass
