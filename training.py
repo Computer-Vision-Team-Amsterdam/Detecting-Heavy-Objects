7dd68af4793ad1d8dd20b0a0d09bb420dfa1f814
Diana Epureanu <dianaepureanu@Dianas-MacBook-Pro.local>
2022-02-21 15:32:00 +0100

    multiple experiments. local outputs file used instead of azure

--- training.py
+++ training.py
@@ -32,63 +32,6 @@ def init_train(flags) -> None:
     CONTAINER_DETECTION_MODEL.train()
 
 
-def collect_nested_lists(dictionary, composed_key, nested_lists):
-    """
-    This method parses a nested dictionary recursively and collects the (composed) keys where the value is a list.
-    :param dictionary:
-    :param composed_key:
-    :param nested_lists:
-
-    :return: keys and values of the @dictionary such that values are of type list.
-    """
-
-    for k, v in dictionary.items():
-        if isinstance(v, dict):
-            if composed_key == "":
-                collect_nested_lists(v, k, nested_lists)
-            else:
-                collect_nested_lists(v, composed_key+"."+k, nested_lists)
-        if isinstance(v, list):
-            if composed_key == "":
-                nested_lists[k] = v
-            else:
-                nested_lists[composed_key+"."+k] = v
-
-    return nested_lists
-
-
-def generate_config_file(file, configuration, name):
-    for composed_name, value in configuration.items():
-        names = composed_name.split(".")
-        if len(names) == 1:
-            file[names[0]] = value
-        if len(names) == 2:
-            file[names[0]][names[1]] = value
-        if len(names) == 3:
-            file[names[0]][names[1]][names[2]] = value
-
-    with open(f'configs/temp_{name}.yaml', 'w') as outfile:
-        yaml.dump(file, outfile, sort_keys=False)
-
-
-def handle_hyperparameters(config):
-
-    # open yaml file as dict
-    with open(config) as f:
-        file = yaml.safe_load(f)
-
-    # get rows for which we do hyperparameter search
-    grid_space = collect_nested_lists(file, "", {})
-
-    count = 0
-    for combination in itertools.product(*grid_space.values()):
-        configuration = dict(zip(grid_space.keys(), combination))
-        generate_config_file(file, configuration, count)
-        count = count + 1
-
-    return count
-
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -98,9 +41,6 @@ if __name__ == "__main__":
 
     flags = parser.parse_args()
 
-    nr_experiments = handle_hyperparameters("configs/hyperparameter-search.yaml")
-
     register_dataset(DATASET_NAME)
-    for exp in trange(nr_experiments):
-        flags.config= f"configs/temp_{exp}.yaml"
-        init_train(flags)
+    print(f"Using {flags.config} config file.")
+    init_train(flags)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """
 This module contains general functionality to handle the annotated data
 """
+import itertools
 import json
 import os
 from pathlib import Path
@@ -8,6 +9,7 @@ from typing import Any, Dict, List, Union
 
 import cv2
 import numpy as np
+import yaml
 from detectron2.structures import BoxMode
 
 
@@ -59,3 +61,61 @@ def get_container_dicts(img_dir: Union[Path, str]) -> List[Dict[str, Any]]:
         record["annotations"] = objs
         dataset_dicts.append(record)
     return dataset_dicts
+
+
+def collect_nested_lists(dictionary, composed_key, nested_lists):
+    """
+    This method parses a nested dictionary recursively and collects the (composed) keys where the value is a list.
+    :param dictionary:
+    :param composed_key:
+    :param nested_lists:
+
+    :return: keys and values of the @dictionary such that values are of type list.
+    """
+
+    for k, v in dictionary.items():
+        if isinstance(v, dict):
+            if composed_key == "":
+                collect_nested_lists(v, k, nested_lists)
+            else:
+                collect_nested_lists(v, composed_key+"."+k, nested_lists)
+        if isinstance(v, list):
+            if composed_key == "":
+                nested_lists[k] = v
+            else:
+                nested_lists[composed_key+"."+k] = v
+
+    return nested_lists
+
+
+def generate_config_file(file, configuration, name):
+
+    for composed_name, value in configuration.items():
+        names = composed_name.split(".")
+        if len(names) == 1:
+            file[names[0]] = value
+        if len(names) == 2:
+            file[names[0]][names[1]] = value
+        if len(names) == 3:
+            file[names[0]][names[1]][names[2]] = value
+
+    with open(f'configs/temp_{name}.yaml', 'w') as outfile:
+        yaml.dump(file, outfile, sort_keys=False)
+
+
+def handle_hyperparameters(config):
+
+    # open yaml file as dict
+    with open(config) as f:
+        file = yaml.safe_load(f)
+
+    # get rows for which we do hyperparameter search
+    grid_space = collect_nested_lists(file, "", {})
+
+    count = 0
+    for combination in itertools.product(*grid_space.values()):
+        configuration = dict(zip(grid_space.keys(), combination))
+        generate_config_file(file, configuration, count)
+        count = count + 1
+
+    return count
