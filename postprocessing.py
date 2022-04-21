1a488afa04c46d66a9000156d263b53c0590e21c
Diana Epureanu <dianaepureanu@Dianas-MacBook-Pro.local>
2022-04-21 14:02:28 +0200

    reformatted filtering large containers

--- postprocessing.py
+++ postprocessing.py
@@ -1,16 +1,54 @@
 """
 This model applies different post processing steps on the results file of the detectron2 model
 """
+import json
+from typing import List
+
 from visualizations.stats import DataStatistics
+from pathlib import Path
 
 
-def discard_objects(predictions: str, smaller_than: float):
+class PostProcessing:
     """
-    Filter out all predictions where area of the object is smaller than @param smaller_than
+    Post processing operations on the output of the predictor
     """
 
-    stats = DataStatistics(json_file=predictions)
-    indices_to_keep = [idx for idx, area in enumerate(stats.areas) if area < smaller_than]
-    predictions_to_keep = [stats.data[idx] for idx in indices_to_keep]
+    def __init__(self, json_predictions: Path,
+                 threshold: float = 20000,
+                 output_path: Path = Path.cwd()/"postprocessed.json"):
+        """
+        Args:
+            :param json_predictions: path to ouput file with predictions from detectron2
+            :param threshold: objects with a bbox smaller and equal to this arg are discarded. value is in pixels
+            :param output_path: where the filtered json with predictions is stored
+            :param predictions_to_keep: list with predictions of the model that we keep throughout postprocessing steps
+        """
+        self.stats = DataStatistics(json_file=json_predictions)
+        self.threshold = threshold
+        self.output_path = output_path
+        self.predictions_to_keep = None
 
-    return predictions_to_keep
+    def filter_by_size(self):
+        """
+        Removes predictions of small objects and writes results to json file
+        """
+
+        def remove_predictions():
+            """
+            Filter out all predictions where area of the bbox of object is smaller and equal to @param threshold pixels
+            """
+            indices_to_keep = [idx for idx, area in enumerate(self.stats.areas) if area > self.threshold]
+            self.predictions_to_keep = [self.stats.data[idx] for idx in indices_to_keep]
+            return self.predictions_to_keep
+
+        def write_json():
+            """
+            Write filtered list of predictions to another json file
+            """
+            with open(self.output_path, "w") as f:
+                json.dump(self.predictions_to_keep, f)
+
+        remove_predictions()
+        write_json()
+        print(self.output_path)
+
--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -1,12 +1,26 @@
+import json
 from unittest import TestCase
-import postprocessing
+from pathlib import Path
+from postprocessing import PostProcessing
+from visualizations.stats import DataStatistics
+
 
 class Test(TestCase):
 
     def __init__(self, *args, **kwargs):
         super(Test, self).__init__(*args, **kwargs)
-        self.results_file = "/Users/dianaepureanu/Documents/Projects/Detecting-Heavy-Objects/outputs/" \
-                        "INFER_2kx4k_resolution_1_Mar-27-01:43/coco_instances_results.json"
+        self.results_file = Path("/Users/dianaepureanu/Documents/Projects/Detecting-Heavy-Objects/outputs/"
+                                 "INFER_2kx4k_resolution_1_Mar-27-01:43/coco_instances_results.json")
 
     def test_discard_objects(self):
-        large_predictions = postprocessing.discard_objects(self.results_file, smaller_than=20000)
+        postprocessing = PostProcessing(json_predictions=self.results_file)
+        postprocessing.filter_by_size()
+
+        filtered_stats = DataStatistics(json_file=postprocessing.output_path)
+        for area in filtered_stats.areas:
+            if area <= postprocessing.threshold:
+                assert False, f"There are still containers smaller than {postprocessing.threshold} pixels" \
+                              f" which have not been discarded."
+
+
+
