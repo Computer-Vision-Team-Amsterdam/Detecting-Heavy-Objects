2bfef2bfdc04e4965099b96c507d24f029513fed
Diana Epureanu <dianaepureanu@Dianas-MacBook-Pro.local>
2022-06-01 08:51:10 +0200

    wrapper works

--- training.py
+++ training.py
@@ -22,33 +22,18 @@ from evaluation import CustomCOCOEvaluator
 from inference import setup_cfg
 from utils import ExperimentConfig, register_dataset
 
-def custom_mapper(dataset_dict):
-    # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
-    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-    image = detection_utils.read_image(dataset_dict["file_name"], format="BGR")
-    transform_list = [T.Resize((800, 1600)),
-                      T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
-                      T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
-                      T.RandomBrightness(intensity_min=0.5, intensity_max=1.5),
-                      T.RandomRotation(angle=[0, 45])]
-    image, transforms = T.apply_transform_gens(transform_list, image)
-    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
-
-    annos = [
-        detection_utils.transform_instance_annotations(obj, transforms, image.shape[:2])
-        for obj in dataset_dict.pop("annotations")
-        if obj.get("iscrowd", 0) == 0
-    ]
-    instances = detection_utils.annotations_to_instances(annos, image.shape[:2])
-    dataset_dict["instances"] = detection_utils.filter_empty_instances(instances)
-    return dataset_dict
+from training_with_augs import custom_mapper_wrapper, custom_mapper, CustomMapper
 
 
 class MyTrainer(DefaultTrainer):
 
     @classmethod
     def build_train_loader(cls, cfg):
-        return build_detection_train_loader(cfg, mapper=custom_mapper)
+        mapper = DatasetMapper(cfg,
+                               is_train=True,
+                               augmentations=[T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
+                                              T.RandomBrightness(intensity_min=0.5, intensity_max=1.5)])
+        return build_detection_train_loader(cfg, mapper=custom_mapper_wrapper)
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
