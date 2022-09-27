23a7abb00587f4c16655ece0c137c420972c3339
Diana <epureanudiana13@gmail.com>
2022-09-27 11:30:25 +0200

    styling for retrieval

--- retrieve_images.py
+++ retrieve_images.py
@@ -1,5 +1,6 @@
 """
-This module retrieves images from CloudVPS, downloads them locally and uploads them to the storage account
+This module retrieves images from CloudVPS,
+downloads them locally and uploads them to the storage account
 The images are downloaded in the `retrieved_images` folder.
 """
 import json
@@ -21,8 +22,9 @@ from requests.auth import HTTPBasicAuth
 
 client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
 credential = ManagedIdentityCredential(client_id=client_id)
-blob_service_client = BlobServiceClient(account_url="https://cvtdataweuogidgmnhwma3zq.blob.core.windows.net",
-                                        credential=credential)
+blob_service_client = BlobServiceClient(
+    account_url="https://cvtdataweuogidgmnhwma3zq.blob.core.windows.net",
+    credential=credential)
 
 airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
 KVUri = airflow_secrets["vault_url"]
@@ -32,18 +34,18 @@ username_secret = client.get_secret(name="CloudVpsRawUsername")
 password_secret = client.get_secret(name="CloudVpsRawPassword")
 socket.setdefaulttimeout(100)
 
-BASE_URL = f"https://3206eec333a04cc980799f75a593505a.objectstore.eu/intermediate/"
+BASE_URL = "https://3206eec333a04cc980799f75a593505a.objectstore.eu/intermediate/"
 USERNAME = username_secret.value
 PASSWORD = password_secret.value
 
 
-def split_pano_id(pano_id: str) -> Tuple[str, str]:
+def split_pano_id(panorama_id: str) -> Tuple[str, str]:
     """
     Splits name of the panorama in TMX* and pano*
     """
-    id_name = pano_id.split("_")[0]
-    index = pano_id.index("_")
-    img_name = pano_id[index + 1:]
+    id_name = panorama_id.split("_")[0]
+    index = panorama_id.index("_")
+    img_name = panorama_id[index + 1:]
     return id_name, img_name
 
 
@@ -62,7 +64,10 @@ def download_panorama_from_cloudvps(
     try:
         url = (
                 BASE_URL
-                + f"{date.year}/{str(date.month).zfill(2)}/{str(date.day).zfill(2)}/{id_name}/{img_name}.jpg"
+                + f"{date.year}/"
+                  f"{str(date.month).zfill(2)}/"
+                  f"{str(date.day).zfill(2)}/"
+                  f"{id_name}/{img_name}.jpg"
         )
 
         response = requests.get(
@@ -82,14 +87,17 @@ def download_panorama_from_cloudvps(
         print(f"{panorama_id} completed.")
 
         path = os.path.join(os.getcwd(), "retrieved_images", f"{panorama_id}.jpg")
-        size = os.path.getsize(path)
+        size = float(os.path.getsize(path))
         size = size / 1000000
         print(f"size in MB of the downloaded file: {size}")
-    except Exception as e:
-        print(f"Failed for panorama {panorama_id}:\n{e}")
+    except requests.HTTPError as exception:
+        print(f"Failed for panorama {panorama_id}:\n{exception}")
 
 
 def upload_to_storage_account(date: str) -> None:
+    """
+    This method uploads images in a specific date sub-blob in the storage account
+    """
     retrieved_images_folder_path = "retrieved_images"
     for file in os.listdir(retrieved_images_folder_path):
         blob_client = blob_service_client.get_blob_client(
