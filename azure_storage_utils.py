e115174252e49a05d57b9fe134f84895cdb729d0
chrise96 <chrise96@hotmail.com>
2022-10-11 10:00:33 +0200

    Feedback verwerkt

--- azure_storage_utils.py
+++ azure_storage_utils.py
@@ -2,7 +2,7 @@ import json
 import os
 from typing import List
 
-from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError
+from azure.core.exceptions import AzureError, ResourceNotFoundError
 from azure.identity import ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobClient, BlobServiceClient, ContainerClient
@@ -27,13 +27,16 @@ class AzureStorageUtils:
 
         try:
             return self.blob_service_client.list_containers()
-        except ResourceExistsError:
-            return list()
+        except AzureError as ex:
+            print("List containers operation failed")
+            raise ex
 
-    def list_container_content(self, cname) -> List[str]:
+    def list_container_content(self, cname: str) -> List[str]:
         """List the content of a container.
         Returns:
             list: A list of all blobs in a container.
+        Args:
+            cname: Name of the Azure Storage Container.
         """
 
         try:
@@ -41,13 +44,16 @@ class AzureStorageUtils:
                 container=cname
             )
             return container_client.list_blobs()
-        except ResourceExistsError:
-            return list()
+        except AzureError as ex:
+            print("List blobs operation failed")
+            raise ex
 
     def upload_blob(self, cname: str, blob_name: str, local_file_path: str) -> None:
         """Upload a file to a container in the cloud.
-        Raises:
-            Exception: Exception that will be raised if the operation fails.
+        Args:
+            cname: Name of the Azure Storage Container.
+            blob_name: Name of the blob in the container.
+            local_file_path: Name given to the file saved locally.
         """
 
         try:
@@ -63,8 +69,10 @@ class AzureStorageUtils:
 
     def download_blob(self, cname: str, blob_name: str, local_file_path: str) -> None:
         """Download a blob from a container.
-        Raises:
-            Exception: Exception that will be raised if the operation fails.
+        Args:
+            cname: Name of the Azure Storage Container.
+            blob_name: Name of the blob in the container.
+            local_file_path: Name given to the file saved locally.
         """
 
         try:
@@ -80,37 +88,10 @@ class AzureStorageUtils:
             print("Failed to download blob.")
             raise ex
 
-    def download_blobs(self, cname: str, cloud_files: List[str]) -> None:
-        """Download certain blobs from a container.
-        Raises:
-            Exception: Exception that will be raised if the operation fails.
-        """
-
-        try:
-            container_client = self.blob_service_client.get_container_client(
-                container=cname
-            )
-            blob_list = container_client.list_blobs()
-
-            for blob in blob_list:
-                if blob.name in cloud_files:
-                    with open(blob.name, "wb") as download_file:
-                        download_file.write(
-                            container_client.get_blob_client(blob)
-                            .download_blob()
-                            .readall()
-                        )
-                    print("download_blob: {} {}".format(cname, blob.name))
-        except ResourceNotFoundError as ex:
-            print("Failed to download blob.")
-            raise ex
-
     def get_secret_client(self) -> SecretClient:
         """Get Azure Secret client.
         Returns:
             SecretClient: Azure secret client.
-        Raises:
-            Exception: Exception that will be raised if the operation fails.
         """
 
         try:
@@ -124,11 +105,9 @@ class AzureStorageUtils:
     def get_secret(self, secret_name: str) -> str:
         """Retrieve secret from cloud.
         Args:
-            secret_name (str): Name of the secret.
+            secret_name: Name of the secret.
         Returns:
             str: The value of the secret.
-        Raises:
-            Exception: Exception that will be raised if the operation fails.
         """
 
         try:
