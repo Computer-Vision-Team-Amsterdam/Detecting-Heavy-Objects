db973c90d196c93d44bbf75432a83746d27d217a
chrise96 <chrise96@hotmail.com>
2022-10-28 11:07:41 +0200

    Update submit_to_sia.py

--- submit_to_sia.py
+++ submit_to_sia.py
@@ -1,10 +1,12 @@
 import socket
 from datetime import datetime
 import requests
+import os
 
 from azure_storage_utils import BaseAzureClient, StorageAzureClient
 
 BASE_URL = "https://acc.api.data.amsterdam.nl/signals/v1/private/signals"
+API_MAX_UPLOAD_SIZE = 20*1024*1024  # 20MB = 20*1024*1024
 
 socket.setdefaulttimeout(100)
 
@@ -30,11 +32,11 @@ def to_signal(text: str, date_now, lat_lng: dict):
     }
 
 def _get_access_token(client_id, client_secret): # TODO change quotes
-    token_url = 'https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token'
+    token_url = "https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token"
     payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'client_credentials'
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
     }
     response = requests.post(token_url, data=payload)
     if response.status_code == 200:
@@ -44,9 +46,6 @@ def _get_access_token(client_id, client_secret): # TODO change quotes
         response.raise_for_status()
 
 def _get_signals_page(headers, page):
-    if access_token is None:
-        raise Exception("Access token cannot be None")
-
     url = BASE_URL + page
     response = requests.get(url, headers=headers)
 
@@ -57,12 +56,10 @@ def _get_signals_page(headers, page):
         return response.raise_for_status()
 
 def _post_signal(headers):
-    if access_token is None:
-        raise Exception("Access token cannot be None")
-
+    # TODO
     text = "CVT Dit is een automatisch gegenereerd signaal."
     date_now: datetime = datetime.now()
-    lat_lng = {"lat": 52.367527, "lng": 4.901257}  # TODO
+    lat_lng = {"lat": 52.367527, "lng": 4.901257}
 
     response = requests.post(
         BASE_URL,
@@ -76,23 +73,25 @@ def _post_signal(headers):
     else:
         return response.raise_for_status()
 
-def image_upload(headers):
-    files = {'file': (file_to_upload, open(file_to_upload, 'rb'))}
+def image_upload(headers, file_to_upload):
+    if os.path.getsize(file_to_upload) > API_MAX_UPLOAD_SIZE:
+        msg = f"File can be a maximum of {API_MAX_UPLOAD_SIZE} bytes in size."
+        raise Exception(msg)
+
+    files = {"file": (file_to_upload, open(file_to_upload, "rb"))}
 
     response = requests.post(url, files=files, headers=headers)
 
     if response.status_code == 200:
         print("The server successfully performed the POST request.")
-        print(response.json())
+        return response.json()
     else:
-        print(response.raise_for_status())
+        return response.raise_for_status()
 
 if __name__ == "__main__":
-    azClient = BaseAzureClient()
-    sia_password = azClient.get_secret_value("sia-password-acc")
-    
+    sia_password = BaseAzureClient().get_secret_value(secret_key="sia-password-acc")
     access_token = _get_access_token("sia-cvt", sia_password)
-    headers = {'Authorization': "Bearer {}".format(access_token)}
+    headers = {"Authorization": "Bearer {}".format(access_token)}
 
     print(_get_signals_page(headers, "?page_size=1"))
     # print(_post_signal(headers))
@@ -106,4 +105,4 @@ if __name__ == "__main__":
     signal_id = "11670"
     url = BASE_URL + f"/{signal_id}/attachments/"
 
-    image_upload(headers)
+    print(image_upload(headers, file_to_upload))
