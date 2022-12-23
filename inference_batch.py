df7b607c8f88bd89faf97f769ba525bca8310b3a
chrise96 <chrise96@hotmail.com>
2022-12-23 11:04:03 +0100

    Klaar

--- inference_batch.py
+++ inference_batch.py
@@ -119,6 +119,42 @@ def instances_to_coco_json(
     return results_json, results
 
 
+def get_chunk_pano_ids():
+    # Download txt file(s) with pano ids that we want to download from CloudVPS
+    local_file = f"{opt.worker_id}.jpg"
+    saClient.download_blob(
+        cname="retrieve-images-input",
+        blob_name=f"{start_date_dag}/{local_file}",
+        local_file_path=local_file,
+    )
+    with open(local_file, "r") as f:
+        pano_ids = [line.rstrip("\n") for line in f]
+
+    if len(pano_ids) < opt.num_workers:
+        raise ValueError("Number of workers is larger than items to process. Aborting...")
+    print(f"Printing first and last file names from the chunk: {pano_ids[0]} {pano_ids[-1]}")
+
+    return pano_ids
+
+
+def download_panos():
+    # Get all file names of the panoramic images from the storage account
+    blobs = saClient.list_container_content(
+        cname="unblurred", blob_prefix=start_date_dag
+    )
+
+    # Validate if all blobs are available
+    if len(set(pano_ids) - set(blobs)) != 0:
+        raise ValueError("Not all panoramic images are available in the storage account! Aborting...")
+
+    for blob in pano_ids:
+        saClient.download_blob(
+            cname="blurred",
+            blob_name=f"{start_date_dag}/{blob}",
+            local_file_path=f"{input_path}/{blob}",
+        )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -126,9 +162,11 @@ if __name__ == "__main__":
     )
     parser.add_argument("--device", type=str, help="Processing on CPU or GPU?")
     parser.add_argument("--weights", type=str, help="Trained weights filename")
+    parser.add_argument("--worker-id", type=int, help="worker ID")
+    parser.add_argument("--num-workers", type=int, help="number of workers")
     opt = parser.parse_args()
 
-    results_file_name = "coco_instances_results.json"
+    results_file_name = f"coco_instances_results_{opt.worker_id}.json"
 
     start_date_dag, start_date_dag_ymd = get_start_date(opt.date)
 
@@ -138,14 +176,9 @@ if __name__ == "__main__":
 
     # Download images from storage account
     saClient = StorageAzureClient(secret_key="data-storage-account-url")
-    blobs = saClient.list_container_content(cname="blurred", blob_prefix=start_date_dag)
-    for blob in blobs:
-        filename = blob.split("/")[-1]  # only get file name, without prefix
-        saClient.download_blob(
-            cname="blurred",
-            blob_name=blob,
-            local_file_path=f"{input_path}/{filename}",
-        )
+
+    pano_ids = get_chunk_pano_ids()
+    download_panos()
 
     cfg = get_cfg()
     cfg.merge_from_file("configs/container_detection.yaml")
