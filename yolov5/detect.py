3de51f6d94cb65e26172e1fa879dee09c0eefad3
chrise96 <chrise96@hotmail.com>
2023-01-06 17:04:43 +0100

    bug fix

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -163,7 +163,7 @@ def run(
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
     if no_save_img:
-        LOGGER.info(f"Results saved to {colorstr('bold', output_folder)}")
+        LOGGER.info(f"Results saved to {colorstr('bold', source)}")
     if update:
         strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
 
