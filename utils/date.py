433f5315d49b58699e2adef97eb4bd0e064daeb0
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-09 15:45:09 +0100

    change api query, retrieved images check for batched dag run

--- utils/date.py
+++ utils/date.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Tuple
+
+
+YMD_FORMAT = "%Y-%m-%d"
 
 
 def get_start_date(arg_date: str) -> Tuple[str, str]:
@@ -10,7 +13,15 @@ def get_start_date(arg_date: str) -> Tuple[str, str]:
     start_date = datetime.strptime(arg_date, "%Y-%m-%d %H:%M:%S.%f")
     my_format = "%Y-%m-%d_%H-%M-%S"
     start_date_dag = start_date.strftime(my_format)
-    my_format_ymd = "%Y-%m-%d"
-    start_date_dag_ymd = start_date.strftime(my_format_ymd)
+    start_date_dag_ymd = start_date.strftime(YMD_FORMAT)
 
     return start_date_dag, start_date_dag_ymd
+
+
+def get_yesterdays_date(arg_date: str) -> str:
+    """Subtract one day from a date string formatted cf. YMD_FORMAT
+    Args:
+        ""arg_date: str = YMD_FORMAT formatted string
+    """
+    _, date_string = get_start_date(arg_date)
+    return (datetime.strptime(date_string, YMD_FORMAT) - timedelta(days=1)).strftime(YMD_FORMAT)
