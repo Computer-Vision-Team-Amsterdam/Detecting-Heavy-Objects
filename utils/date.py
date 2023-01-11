ae6e0304af53cc52a602393037b2826321c33f7d
chrise96 <chrise96@hotmail.com>
2023-01-11 09:59:16 +0100

    Update date.py

--- utils/date.py
+++ utils/date.py
@@ -16,12 +16,3 @@ def get_start_date(arg_date: str) -> Tuple[str, str]:
     start_date_dag_ymd = start_date.strftime(YMD_FORMAT)
 
     return start_date_dag, start_date_dag_ymd
-
-
-def get_yesterdays_date(arg_date: str) -> str:
-    """Subtract one day from a date string formatted cf. YMD_FORMAT
-    Args:
-        ""arg_date: str = YMD_FORMAT formatted string
-    """
-    _, date_string = get_start_date(arg_date)
-    return (datetime.strptime(date_string, YMD_FORMAT) - timedelta(days=1)).strftime(YMD_FORMAT)
