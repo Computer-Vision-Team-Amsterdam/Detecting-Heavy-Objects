af4fe51ad06d33efe008af9c0132454944e93caf
chrise96 <chrise96@hotmail.com>
2023-01-26 11:45:26 +0100

    Update detect.py

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -233,10 +233,12 @@ def download_panos():
 
     # Validate if all blobs are available
     pano_ids_txt = [f"{start_date_dag}/{item}.jpg" for item in pano_ids]
-    if len(set(pano_ids_txt) - set(blobs)) != 0:
-        raise ValueError("Not all panoramic images are available in the storage account! Aborting...")
+    # if len(set(pano_ids_txt) - set(blobs)) != 0:
+    #     raise ValueError("Not all panoramic images are available in the storage account! Aborting...")
 
-    for blob in pano_ids_txt:
+    pano_ids_new = list(set(pano_ids_txt) & set(blobs))
+
+    for blob in pano_ids_new:
         filename = blob.split("/")[1]
         saClient.download_blob(
             cname="unblurred",
