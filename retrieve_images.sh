6ccf75fd2e5e39b7108222280bc4af9bc860b56b
chrise96 <chrise96@hotmail.com>
2023-01-26 08:08:31 +0100

    Update retrieve_images.sh

--- retrieve_images.sh
+++ retrieve_images.sh
@@ -40,7 +40,7 @@ rclone config create azureblob_rclone_twee azureblob container=$containerNameTwe
 # Set the source and destination directories
 src_dir=objectstore_rclone:panorama/$cloudvps_folder
 dst_dir=azureblob_rclone:unblurred/$azure_folder/
-
+dst_dir2=azureblob_rclone_twee:retrieve-images-input/$azure_folder
 
 # Get directory structure for source dir, and remove the first line ("/") and stripansi
 rclone tree $src_dir --noindent --include "equirectangular/panorama_8000.jpg" --noreport | sed -e '1,1d' -e 's/\x1B\[[0-9;]*[JKmsu]//g' > files.txt
@@ -60,61 +60,49 @@ awk '{
 # Convert Cloud VPS paths to pano ids
 sed 's/\/equirectangular\/panorama_8000.jpg//' paths.txt | tr '/' '_' > pano_ids.txt
 
-counter=0
 ## Loop over the combined paths and panorama id files to get the source and destination file path pairs
 #paste paths.txt pano_ids.txt | while read path pano_id; do
 #   rclone copyto $src_dir/$path $dst_dir$pano_id.jpg \
 #      --azureblob-use-msi \
 #      --azureblob-msi-client-id=$USER_ASSIGNED_MANAGED_IDENTITY;
-#
-#      counter=$((counter+1))
-#      if [ $counter -eq 10 ]; then
-#          break
-#      fi
 #done
 
+counter=0
 while read line; do
     # Convert Cloud VPS paths to pano ids
-    newline=$(echo "$line" | sed 's/\/equirectangular\/panorama_8000.jpg//' | tr '/' '_')
+    newline=$(echo "$line" | sed 's/\/equirectangular\/panorama_8000.jpg//' | tr '/' '_') # TODO already defined
     echo $newline
     rclone copyto "$src_dir/$line" "$dst_dir$newline.jpg" \
     --azureblob-use-msi \
-    --azureblob-msi-client-id=$USER_ASSIGNED_MANAGED_IDENTITY;
+    --azureblob-msi-client-id=$USER_ASSIGNED_MANAGED_IDENTITY \
+    --verbose
 
     # check the exit status of the rclone copyto command after every iteration
     if [ $? -ne 0 ]; then
         echo "Error: Failed to copy $line to $dst_dir$newline.jpg"
     fi
 
-    counter=$((counter+1))
+    counter=$((counter+1)) # TODO remove
     if [ $counter -eq 10 ]; then
         break
     fi
 done < paths.txt
 
-num_workers=8 # TODO
+num_workers=8 # TODO use arg
 chunk_folder="splits"
 mkdir $chunk_folder
-
-dst_dir2=azureblob_rclone_twee:retrieve-images-input/$azure_folder
 
 split -n $num_workers pano_ids.txt $chunk_folder/
 
 i=1
-for file in $chunk_folder/*; do
-    mv "$file" "$chunk_folder/$i.txt"
-    i=$((i+1))
-done
-
-for chunk_file in "$chunk_folder/*"
-do
-    base_name=$chunk_file
-    echo $base_name
-    rclone copyto $chunk_file $dst_dir2/$base_name \
+for chunk_file in $chunk_folder/*; do
+    rclone copyto $chunk_file $dst_dir2/$i \
         --azureblob-use-msi \
-        --azureblob-msi-client-id=$USER_ASSIGNED_MANAGED_IDENTITY
+        --azureblob-msi-client-id=$USER_ASSIGNED_MANAGED_IDENTITY \
+        --verbose
     # check the exit status of the rclone copyto command after every iteration
     if [ $? -ne 0 ]; then
         echo "Error: Failed to copy $chunk_file to $dst_dir2/$base_name"
     fi
+    i=$((i+1))
 done
