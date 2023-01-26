775af45de90b230de6716aaef15d7f2850b904b7
chrise96 <chrise96@hotmail.com>
2023-01-26 16:02:46 +0100

    Print non matching lists

--- inference_batch.py
+++ inference_batch.py
@@ -147,10 +147,10 @@ def download_panos() -> None:
 
     # Validate if all blobs are available
     pano_ids_txt = [f"{start_date_dag}/{item}.jpg" for item in pano_ids]
-    # if len(set(pano_ids_txt) - set(blobs)) != 0:
-    #     raise ValueError(
-    #         "Not all panoramic images are available in the storage account! Aborting..."
-    #     )
+    if len(set(pano_ids_txt) - set(blobs)) != 0:
+        print(
+            "Not all panoramic images are available in the storage account! Finding the matching now..."
+        )
     pano_ids_new = list(set(pano_ids_txt) & set(blobs))
 
     for blob in pano_ids_new:
